9fde97d07a3cd091c5f9c7a4831ec8fd07c54be4
muanziri <munaziribienaime@yahoo.com>
2022-05-10 07:49:50 +0000

    make it better

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ app.use('/static',express.static(__dirname+'/static'))
 app.get('/',(req,res)=>{
     res.render('index')
 })
-
+app.get('/podcast',(req,res)=>{
+    res.render('podcast')
+})
+app.get('/VideoEmbeds',(req,res)=>{
+    res.render('VideoEmbeds')
+})
 
 app.listen(3000,()=>{
     console.log('heard from 3000');
